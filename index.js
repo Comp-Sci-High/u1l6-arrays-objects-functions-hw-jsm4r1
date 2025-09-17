d7d12b2891dfd0da88e0592ac355b5eb9fa1183c
jsm4r1 <mariam.ozuna26@compscihigh.org>
2025-09-17 16:35:04 +0000

    create arrays, objects, and functions.

--- index.js
+++ index.js
@@ -9,7 +9,12 @@
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
 
-
+let fruits = ["apple","banana","cherry"]
+console.log(fruits[1])
+fruits[2] = "orange"
+fruits.push("grape")
+fruits.pop()
+console.log(fruits)
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
@@ -19,12 +24,24 @@
 // E. Delete the `age` property from the object.
 // F. Log the updated student object.
 
-
+let student = {
+   name: "Mari",
+   age: '17',
+   grade: "A"
+}
+console.log(student.age)
+student.grade = "A+"
+student.push(isGraduated)
+student.isGraduated = "true"
+delete student.age
+console.log(student)
 
 // TASK 3: Functions - Create and Call Functions
 // A. Write a function called `calculateTotal` that takes two inputs: price and quantity.
 //    The function should return the total price by multiplying price by quantity.
-
+function calculateTotal(price, quality){
+   return price * quantity
+}
 
 
 // B. Call the `calculateTotal` function 3 times and console log the outputs
@@ -32,6 +49,9 @@
 // Second with the inputs 12 and 8
 // Third with the inputs 9 and 42
 
+calculateTotal(20,5)
+calculateTotal(12,8)
+calculateTotal(9,42)
 
 /*
    ==== Final Commit Instructions ====
